a07bba664f46e40e55a1efec0d2af353112d88b8
Liam Demafelix <hello@liam.ph>
2018-12-25 19:34:43 +0800

    Initial upload

---
+++ partials/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php if (empty($title)) echo "The Undernet"; else echo $title . " - The Undernet"; ?></title>
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/css/prism.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-lg-8 col-lg-offset-2">
+                <div class="page-header text-center">
+                    <img src="/images/avatar.png" class="blog-logo">
+                    <h1><a href="/">Liam Demafelix</a></h1>
+                    <p>Hello! My name is Liam, and I'm a Software Engineer based in Manila.</p>
+                    <p class="mt-2">
+                        <a href="/about" class="mr-1"><i class="fa fa-user fa-fw"></i> About Me</a>
+                        <a href="/contact" class="mr-1"><i class="fa fa-envelope fa-fw"></i> Contact</a>
+                        <a href="https://github.com/liamdemafelix" class="mr-1"><i class="fa fa-github fa-fw"></i> GitHub</a>
+                        <a href="https://webapps.space/liam/welcome" class="mr-1"><i class="fa fa-gitlab fa-fw"></i> GitLab</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-lg-8 col-lg-offset-2">
